16b4a89c907864399fd12e02f2c11da0b8942682
Ryan Bak <ryanmbak@gmail.com>
2023-12-15 15:51:44 -0500

    Remove hooks and rename module script

--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,4 @@
 export default class Simple {
-
   static getSelected() {
   	const controlled = canvas.tokens.controlled
   	if (controlled.length < 1) {
@@ -12,13 +11,3 @@ export default class Simple {
     return controlled[0];
   }
 } 
-
-Hooks.on('ready', () => {
-  window.Simple = Simple;
-});
-
-Hooks.on('canvasReady', (canvas) => {
-  if (!window.hasOwnProperty('Simple')) {
-    window.Simple = Simple;
-  }
-});
