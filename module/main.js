715aa37c2b8205bbedbc2a7f5cfb13c3ec9179c8
Ryan Bak <ryanmbak@gmail.com>
2023-12-15 10:49:10 -0500

    Initial test

---
+++ module/main.js
@@ -0,0 +1,14 @@
+export default class Simple {
+
+  static getSelected() {
+  	const controlled = canvas.tokens.controlled
+  	if (controlled.length < 1) {
+  		ui.notifications.error('No objects are selected');
+  		throw "No objects selected";
+  	}
+  	if (controlled.length > 1) {
+  		ui.notifications.warn('More than one object is selected. Only one object will be used.');
+  	}
+    return controlled[0];
+  }
+}
